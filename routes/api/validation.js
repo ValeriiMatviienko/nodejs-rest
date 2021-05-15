1a86d0852c590d0d867341dca7d1bc6be3499967
ValeriiMatviienko <unanimous276@gmail.com>
2021-05-15 13:05:17 +0300

    add mongoose and schema

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,19 +1,11 @@
 const Joi = require("joi");
 
-const schemaCreateContact = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string()
-    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[-][\d]{4}/)
-    .required(),
-});
-
-const schemaUpdateContact = Joi.object({
-  name: Joi.string().min(3).max(30).optional(),
-  email: Joi.string().email().optional(),
-  phone: Joi.string()
-    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[-][\d]{4}/)
-    .optional(),
+const schemaValidateContact = Joi.object({
+  name: Joi.string().min(3).max(30),
+  email: Joi.string().email(),
+  phone: Joi.string(),
+  password: Joi.string().min(7).max(15),
+  subscription: Joi.string(),
 });
 
 const validate = (schema, obj, next) => {
@@ -27,10 +19,6 @@ const validate = (schema, obj, next) => {
   next();
 };
 
-module.exports.createContact = (req, _res, next) => {
-  return validate(schemaCreateContact, req.body, next);
-};
-
-module.exports.updateContact = (req, _res, next) => {
-  return validate(schemaUpdateContact, req.body, next);
+module.exports.validateContact = (req, _res, next) => {
+  return validate(schemaValidateContact, req.body, next);
 };
