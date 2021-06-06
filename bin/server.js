a7bd740b017b5ccd40b1c979ae2351473f9a98b0
ValeriiMatviienko <unanimous276@gmail.com>
2021-06-06 14:49:07 +0300

    add test

--- bin/server.js
+++ bin/server.js
@@ -1,13 +1,19 @@
 const app = require("../app");
 const db = require("../model/db");
+const createFolderIsExist = require("../service/create-dir");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    const UPLOAD_DIR = process.env.UPLOAD_DIR;
+    const USERS_AVATARS = process.env.USERS_AVATARS;
+    await createFolderIsExist(UPLOAD_DIR);
+    await createFolderIsExist(USERS_AVATARS);
     console.log(`Server running. Use our API on port: ${PORT}`);
   });
 }).catch((err) => {
-  console.log(`Server isn't running. Error message: ${err.message}`);
+  console.log(`Server not running. Error message: ${err.message}`);
   process.exit(1);
 });
